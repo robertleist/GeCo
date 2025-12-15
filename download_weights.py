4fde23657c1d9014e938f607e526f88b50e0b46f
Robert Leist <robert.leist@dfki.de>
2025-12-15 09:44:32 +0100

    Adding script for downloading the weights

---
+++ download_weights.py
@@ -0,0 +1,36 @@
+import os
+
+from paths import WEIGHTS_FILE, WEIGHTS_DOWNLOAD_URL, WEIGHTS_DIR
+from os.path import join, exists
+from requests import get
+from tqdm import tqdm
+
+def download_weights(force_download=False):
+    os.makedirs(WEIGHTS_DIR, exist_ok=True)
+    if exists(WEIGHTS_FILE) and not force_download:
+        print('weights file already exists, skipping download')
+        return
+
+    print(f'Downloading weights from {WEIGHTS_DOWNLOAD_URL}...')
+    response = get(WEIGHTS_DOWNLOAD_URL, stream=True)
+    response.raise_for_status()  # Raise an error for bad status codes
+
+    # Get the total file size from the response headers
+    total_size = int(response.headers.get('content-length', 0))
+
+    with open(WEIGHTS_FILE, 'wb') as f, tqdm(
+            desc=f"Downloading GeCo weights to: {WEIGHTS_FILE}",
+            total=total_size,
+            unit='B',
+            unit_scale=True,
+            unit_divisor=1024,
+    ) as bar:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+            bar.update(len(chunk))
+
+    print('Download completed successfully.')
+
+
+if __name__ == '__main__':
+    download_weights()
